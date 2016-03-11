2752d0c7795f4e791be71db912edb18cd151ae82
Nick Breslin <nickbreslin@gmail.com>
2016-03-11 15:38:31 -0800

    Loop Animation script

---
+++ Assets/LoopAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoopAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+					gameObject.GetComponent<Animation>().wrapMode = WrapMode.Loop;
+
+			//PlayAnimation();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
